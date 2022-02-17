bbe7041e32e73e93391aadb7ec89f3a73b7462ee
lingar <yimprogramming@gmail.com>
2022-02-17 21:15:58 +0200

    To fix the years problem

--- src/moviesComps/appMovies.js
+++ src/moviesComps/appMovies.js
@@ -1,62 +1,81 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from "axios";
-import { sortBy } from "lodash"
+import {sortBy} from "lodash"
 import "./css/all.css"
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
-import { MoviesInput } from './moviesInput'
-import { StripHome } from './stripHome';
-import { MoviesList } from './moviesList'
-import { MovieVideo } from './movieVideo'
-
-
+import {MoviesInput} from './moviesInput'
+import {StripHome} from './stripHome';
+import {MoviesList} from './moviesList'
+import {MovieVideo} from './movieVideo'
 
 
 export const AppMovies = () => {
-  // default is Moives bank
-  let [search, setSearch] = useState("bank")
-  let [ar, setAr] = useState([])
-  let [sortSelect, setSortSelect] = useState("Title");
+    // default is Moives bank
+    let [search, setSearch] = useState("bank")
+    let [ar, setAr] = useState([])
+    let [sortSelect, setSortSelect] = useState("Title");
 
 
-  useEffect(() => {
-    doSearchApi();
-  }, [search])
+    useEffect(() => {
+        doSearchApi();
+    }, [search])
+//NatanE
+    const sortMovies = (_sort) => {
+        // https://www.geeksforgeeks.org/lodash-_-sortby-method/
+        console.log("_sort = ", _sort);
+        //TODO -more conditions for years
 
-  const sortMovies = (_sort) => {
-    let temp_ar = sortBy(ar, _sort);
-    setAr(temp_ar);
-
-  }
-  //  axios.get("https://www.omdbapi.com/?s=bank&y=2020&apikey=e7c61f3b")
+                //if .... Title || Year? - do this
+                let temp_ar = sortBy(ar, _sort);
 
 
+                //else - specific year...
+                //build the api, call to axios, as You did in the search -- see the doSearchApi below
+                //the apihttps://www.omdbapi.com/?s=bank&y=2020&apikey=000000
 
-  const doSearchApi = async () => {
-    let url = `http://www.omdbapi.com/?s=${search}&apikey=e7c61f3b`
-    // let resp = await fetch(url);
-    // let data = await resp.json();
-    let resp = await axios.get(url);
-    console.log("axios",resp);
-    let temp_ar = sortBy(resp.data.Search, sortSelect);
-    setAr(temp_ar)
-  }
+                // let url = `http://www.omdbapi.com/?s=${search}&apikey=e7c61f3b`
+                // // let resp = await fetch(url);
+                // // let data = await resp.json();
+                // let resp = await axios.get(url);
+                // console.log("axios", resp);
 
-  return (
-    <Router>
-      <MoviesInput sortMovies={sortMovies} setSearch={setSearch} />
-      <Switch>
-        <Route exact path="/" render={() =>
-          <div>
-            <StripHome />
-            <MoviesList movies_ar={ar} />
-          </div>
-        } />
-       <Route exact path="/Video/:id" component={MovieVideo}/>
 
-      </Switch>
+                //optional
+                // let temp_ar = sortBy(ar, "Title";
 
-    </Router>
-  )
+
+        setAr(temp_ar);
+
+    }
+    //  axios.get("https://www.omdbapi.com/?s=bank&y=2020&apikey=e7c61f3b")
+
+
+    const doSearchApi = async () => {
+        let url = `http://www.omdbapi.com/?s=${search}&apikey=e7c61f3b`
+        // let resp = await fetch(url);
+        // let data = await resp.json();
+        let resp = await axios.get(url);
+        console.log("axios", resp);
+        let temp_ar = sortBy(resp.data.Search, sortSelect);
+        setAr(temp_ar)
+    }
+
+    return (
+        <Router>
+            <MoviesInput sortMovies={sortMovies} setSearch={setSearch}/>
+            <Switch>
+                <Route exact path="/" render={() =>
+                    <div>
+                        <StripHome/>
+                        <MoviesList movies_ar={ar}/>
+                    </div>
+                }/>
+                <Route exact path="/Video/:id" component={MovieVideo}/>
+
+            </Switch>
+
+        </Router>
+    )
 }
 
--- src/moviesComps/moviesInput.js
+++ src/moviesComps/moviesInput.js
@@ -1,38 +1,45 @@
-import React, { useRef } from "react";
+import React, {useRef} from "react";
 // import { useHistory } from "react-router";
 
 export const MoviesInput = (props) => {
     let inputRef = useRef();
     let sortRef = useRef();
     // let history = useHistory()
-  return (
-    <div className="container-fluid bg-danger p-3">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4">
-            <h2>Movies search</h2>
-          </div>
-          <div className="col-lg-8 d-flex align-items-center justify-content-end ">
-            <input ref={inputRef} placeholder="Search...." type="search"className="form-control w-25"/>
-            <button onClick={() =>{
-              // history.push("/")
-                props.setSearch(inputRef.current.value)
-            } } className="btn btn-dark"> Search</button>
-            <div className="mx-2">Sort:</div>
-            <select onChange={()=>{
-               props.sortMovies(sortRef.current.value) 
-            }} ref={sortRef} className="from-select w-25">
-              <option value="Title">name</option>
-              <option value="Year">year</option>
-            <option value="Year">Year : 2021</option>
-              <option value="Year">Year : 2020</option>
-              <option value="Year">year : 2019</option>
-              <option value="Year">year : 2018</option>
-            </select>
-          </div>
+    return (
+        <div className="container-fluid bg-danger p-3">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4">
+                        <h2>Movies search</h2>
+                    </div>
+                    <div className="col-lg-8 d-flex align-items-center justify-content-end ">
+                        <input ref={inputRef} placeholder="Search...." type="search" className="form-control w-25"/>
+                        <button onClick={() => {
+                            // history.push("/")
+                            props.setSearch(inputRef.current.value);
+                        }} className="btn btn-dark"> Search
+                        </button>
+                        <div className="mx-2">Sort:</div>
+
+                        <select onChange={() => {
+                            props.sortMovies(sortRef.current.value)
+                        }} ref={sortRef} className="from-select w-25">
+
+
+                            <option value="Title">name</option>
+                            <option value="Year">year</option>
+                            <option value="Year">Year : 2021</option>
+                            <option value="Year">Year : 2020</option>
+                            <option value="Year">year : 2019</option>
+                            <option value="Year">year : 2018</option>
+                            <option value="2021">4324  </option>
+
+
+                        </select>
+                    </div>
+                </div>
+
+            </div>
         </div>
-       
-      </div>
-    </div>
-  )
+    )
 }
