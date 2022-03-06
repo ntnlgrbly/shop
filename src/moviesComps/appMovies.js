0db1c15862b6655303ed9d6149c206919bfbd147
lingar <yimprogramming@gmail.com>
2022-03-06 19:02:36 +0200

    More fixes

--- src/moviesComps/appMovies.js
+++ src/moviesComps/appMovies.js
@@ -1,76 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from "axios";
-import { sortBy } from "lodash"
+import {sortBy} from "lodash"
 import "./css/all.css"
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
-import { MoviesInput } from './moviesInput'
-import { StripHome } from './stripHome';
-import { MoviesList } from './moviesList'
-import { MovieVideo } from './movieVideo'
-
-
+import {MoviesInput} from './moviesInput'
+import {StripHome} from './stripHome';
+import {MoviesList} from './moviesList'
+import {MovieVideo} from './movieVideo'
 
 
 export const AppMovies = () => {
-  // default is Moives bank
-  let [search, setSearch] = useState("bank")
-  let [ar, setAr] = useState([])
-  let [sortSelect, setSortSelect] = useState("Title");
+    // default is Moives bank
+    // let [search, setSearch] = useState("bank")
+    let [ar, setAr] = useState([])
+    let [sortSelect, setSortSelect] = useState("Title");
+    //
+    //
+    // useEffect(() => {
+    //     doSearchApi();
+    // }, [search])
 
+    const sortMovies = (_sort) => {
+        let temp_ar = sortBy(ar, _sort);
+        setAr(temp_ar);
 
-  useEffect(() => {
-    doSearchApi();
-  }, [search])
+    }
+    //  axios.get("https://www.omdbapi.com/?s=bank&y=2020&apikey=e7c61f3b")
 
-  const sortMovies = (_sort) => {
-    let temp_ar = sortBy(ar, _sort);
-    setAr(temp_ar);
+    //
+    // const doSearchApi = async () => {
+    //     let url = `http://www.omdbapi.com/?s=${search}&apikey=e7c61f3b`
+    //     // let resp = await fetch(url);
+    //     // let data = await resp.json();
+    //     let resp = await axios.get(url);
+    //     console.log("axios", resp);
+    //     let temp_ar = sortBy(resp.data.Search, sortSelect);
+    //     setAr(temp_ar)
+    // }
 
-  }
-  //  axios.get("https://www.omdbapi.com/?s=bank&y=2020&apikey=e7c61f3b")
-
-
-
-  const doSearchApi = async () => {
-    let url = `http://www.omdbapi.com/?s=${search}&apikey=e7c61f3b`
-    // let resp = await fetch(url);
-    // let data = await resp.json();
-    let resp = await axios.get(url);
-    console.log("axios",resp);
-    let temp_ar = sortBy(resp.data.Search, sortSelect);
-    setAr(temp_ar)
-  }
-
-
-
+    // http://localhost:3000/abc
     const doSearchApi2 = async (searchQuery) => {
         let url = `http://www.omdbapi.com/?s=${searchQuery}&apikey=e7c61f3b`
         // let resp = await fetch(url);
         // let data = await resp.json();
         let resp = await axios.get(url);
-        console.log("axios",resp);
+        console.log("axios", resp);
         let temp_ar = sortBy(resp.data.Search, sortSelect);
-        setAr(temp_ar)
+        setAr(temp_ar);
     }
 
-  return (
-    <Router>
-      <MoviesInput sortMovies={sortMovies} setSearch={setSearch} />
-      <Switch>
-        <Route exact path={["/:search", "/"]} render={(data) =>
-            // <Route exact path="/" render={(data) =>
+    return (
+        <Router>
+            <MoviesInput sortMovies={sortMovies}  />
+            <Switch>
+                <Route exact path={["/:search", "/"]} render={(data) =>
+                    // <Route exact path="/" render={(data) =>
 
-                <div>
-            <StripHome />
-            <MoviesList movies_ar={ar} doSearchApi2 = {doSearchApi2} {...data}/>
-          </div>
-        } />
-       <Route exact path="/Video/:id" component={MovieVideo}/>
+                    <div>
+                        <StripHome/>
+                        <MoviesList movies_ar={ar}
+                                    doSearchApi2={doSearchApi2} {...data}/>
+                    </div>
+                }/>
+                <Route exact path="/Video/:id" component={MovieVideo}/>
 
-      </Switch>
+            </Switch>
 
-    </Router>
-  )
+        </Router>
+    )
 }
 
